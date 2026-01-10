5ff3f1f2570accf3eb10e44ecdf1682667d0fcfe
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2026-01-11 04:26:53 +0600

    initial router setup

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Footer from '../pages/Shared/Footer/Footer';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+
+const RootLayout = () => {
+    return (
+      <div className="flex flex-col min-h-screen">
+        <header>
+          <Navbar />
+        </header>
+        <main className="flex-1">
+          <Outlet></Outlet>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    );
+};
+
+export default RootLayout;
