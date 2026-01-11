334a584415c20b2d8e0a35cd5507e40e7a55a46a
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2026-01-11 18:36:59 +0600

    customize theme and design footer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,7 +5,7 @@ import Navbar from '../pages/Shared/Navbar/Navbar';
 
 const RootLayout = () => {
     return (
-      <div className="flex flex-col min-h-screen">
+      <div className="max-w-7xl mx-auto flex flex-col min-h-screen">
         <header>
           <Navbar />
         </header>
