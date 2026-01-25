894347f4a82038c933bb99c2823f05a72dd58433
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2026-01-25 15:23:26 +0600

    update parcel info after payment is confirmed

--- src/pages/Dashboard/MyParcels/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/MyParcels/Payment/PaymentSuccess.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
-    return (
-        <div>
-            <h2 className="text-4xl">Payment Successful</h2>
-        </div>
-    );
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+  console.log(sessionId);
+  const axiosSecure = useAxiosSecure();
+
+  useEffect(() => {
+    if (sessionId) {
+      axiosSecure
+        .patch(`/payment-success?session_id=${sessionId}`)
+        .then((res) => {
+          console.log(res.data);
+        });
+    }
+  }, [sessionId, axiosSecure]);
+
+  return (
+    <div>
+      <h2 className="text-4xl">Payment Successful</h2>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
